72b3ce8b99345fd55a3cab87d181b1d2a9edb6c5
kirabelmont <38549865+kirabelmont@users.noreply.github.com>
2022-12-21 07:34:16 -0600

    cambios del 21

---
+++ lib/screens/hombre/hombre.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HombrePage extends StatelessWidget {
+  const HombrePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
