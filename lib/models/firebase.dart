4792e6745c1d7586a9c68ab1e940c81248cae609
kirabelmont <38549865+kirabelmont@users.noreply.github.com>
2022-12-30 19:05:11 -0600

    canmbios del firebase

---
+++ lib/models/firebase.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+Future<List> getProducto() async {
+  List productos = [];
+
+  CollectionReference collectionReferenceProductos = db.collection('productos');
+  QuerySnapshot queryproductos = await collectionReferenceProductos.get();
+
+  queryproductos.docs.forEach((documento) {
+    productos.add(documento.data());
+  });
+
+  return productos;
+}
